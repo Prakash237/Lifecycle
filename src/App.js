cbcccc0db63c8c5c7cea661b344efaad171dad17
Prakash Shivaji Deshmukh <prakash.deshmukh@telusinternational.com>
2023-05-03 23:05:37 +0530

    second commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+// import ClassComponent from './components/C_F_Components/classComponent'
+// import FunctionalComponent from './components/C_F_Components/functionalComponent'
+//import ParentComponent from './components/C_F_Components/Component/ParentComponent'
+// import ConstructorMethod from './components/mounting/constructorMethod'
+// import GetDerivedStateFromPropsMethod from './components/mounting/getDerivedStateFromPropsMethod'
+// import RenderMethod from './components/mounting/renderMethod'
+// import ComponentDidMountMethod from './components/mounting/componentDidMountMethod'
+// import ShouldComponentUpdateMethod from './components/updating/shouldComponentUpdateMethod'
+// import GetSnapshotBeforeUpdateMethod from './components/updating/getSnapshotBeforeUpdateMethod'
+// import ComponentDidUpdateMethod from './components/updating/componentDidUpdateMethod'
+// import ComponentWillUnmountMethod from './components/unmounting/componentWillUnmount'
+
+export default class App extends Component {
+  render() {
+    return (
+      <>
+      {/* <h1>Mounting</h1>
+      <h2>constructor Method</h2>
+       <ConstructorMethod />
+       <h2>getDerivedStateFromProps Method</h2>
+       <GetDerivedStateFromPropsMethod />
+       <h2>render Method</h2>
+       <RenderMethod />
+       <h2>componentDidMount Method</h2>
+       <ComponentDidMountMethod/>
+       <h1>Updating</h1>
+       <h2>shouldComponentUpdate Method</h2>
+       <ShouldComponentUpdateMethod />
+       <h2>getSnapshotBeforeUpdate Method</h2>
+       <GetSnapshotBeforeUpdateMethod />
+       <h2>componentDidUpdate Method</h2>
+       <ComponentDidUpdateMethod />
+       <h1>Unmounting</h1>
+       <h2>componentWillUnmount Method </h2>
+       <ComponentWillUnmountMethod /> */}
+       {/* <h2>ClassComponent</h2>
+       <ClassComponent />
+       <h2>FunctionalComponent</h2>
+       <FunctionalComponent />  */}
+       {/* <h1>-----------PROPS-------------</h1>
+      <ParentComponent />  */}
+      </>
+    )
+  }
+}
+
